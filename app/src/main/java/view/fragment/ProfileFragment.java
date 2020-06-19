2e0ddf6c40a55d0e5a49930392807114efbcd30a
Jean <jeansam10100100@gmail.com>
2020-06-19 16:17:46 -0500

    FragmentProfile con pictureuser redondeada terminado

--- app/src/main/java/view/fragment/ProfileFragment.java
+++ app/src/main/java/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.platzigram.Adapter.PictureAdapterRecyclerView;
 import com.example.platzigram.R;
+import com.example.platzigram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +32,37 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view=inflater.inflate(R.layout.fragment_profile, container, false);
+        showToolbar("",false,view);
+
+        RecyclerView picturesRecycler=view.findViewById(R.id.pictureRecyclerProfile);
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView=new PictureAdapterRecyclerView(buidPictures(),R.layout.cardview_picture,getActivity());
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public ArrayList<Picture> buidPictures(){
+        ArrayList<Picture> pictures=new ArrayList<>();
+        pictures.add(new Picture("https://images.pexels.com/photos/821406/pexels-photo-821406.jpeg","Dariana","2 dias","4 Me gusta"));
+        pictures.add(new Picture("https://i.ibb.co/4Rx3FWf/94887700-546697096242026-6649823046066503680-n.jpg","Joselyn","5 dias","5 Me gusta"));
+        pictures.add(new Picture("https://i.imgur.com/vCAtpkU.jpg","Cataleya","4 dias","2 Me gusta"));
+
+        return pictures;
+    }
+
+    public void showToolbar(String tittle,boolean upButton,View view){
+
+        Toolbar toolbar=view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
     }
 }
